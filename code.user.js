7ff8c73a0d435464eca47684b8a14cd9d85b8e75
Mateus Z3 <mateuszanatta1@hotmail.com>
2021-01-06 12:48:11 -0300

    Updated script

--- code.user.js
+++ code.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/MateusZ36
 // @match       https://gamesdonequick.com/schedule
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      MateusZ3
 // @homepageURL https://github.com/MateusZ36/GDQ-SChedule-Marker
 // @downloadURL https://github.com/MateusZ36/GDQ-SChedule-Marker/raw/main/code.user.js
@@ -11,14 +11,29 @@
 // @description Highlights the current run in the event
 // ==/UserScript==
 
+function GM_addStyle(cssStr) {
+    var D = document;
+    var newNode = D.createElement ('style');
+    newNode.textContent = cssStr;
+
+    var targ = D.getElementsByTagName ('head')[0] || D.body || D.documentElement;
+    targ.appendChild (newNode);
+}
+
+GM_addStyle(`
+  .current {
+    background-color: #b3ff99;
+    font-weight: bold;
+  }
+`)
+
 const table_rows = $("#runTable tbody tr")
 const data_atual = new Date()
 
 for(i=0;i<table_rows.length;i++){
   if(table_rows[0].className == ""){
     if(new Date($(table_rows[i]).children('td.start-time').html()) > data_atual){
-      $(table_rows[i-1]).css("background-color","#b3ff99")
-      $(table_rows[i-2]).css("background-color","#b3ff99")
+      $(table_rows[i-2]).addClass("current").next().addClass("current")
       break
     }
   }
