4fe7ef0d303e28d6f9df3f6a21b1c30ae55dedf8
Mateus Z3 <mateuszanatta1@hotmail.com>
2021-01-06 17:05:59 -0300

    Update code.user.js

--- code.user.js
+++ code.user.js
@@ -3,14 +3,24 @@
 // @namespace   https://github.com/MateusZ36
 // @match       https://gamesdonequick.com/schedule
 // @grant       none
-// @version     1.2
+// @version     1.3
 // @author      MateusZ3
 // @homepageURL https://github.com/MateusZ36/GDQ-SChedule-Marker
 // @downloadURL https://github.com/MateusZ36/GDQ-SChedule-Marker/raw/main/code.user.js
 // @updateURL   https://github.com/MateusZ36/GDQ-SChedule-Marker/raw/main/code.user.js
 // ==/UserScript==
 
-
+Date.prototype.sum_str = function(time) {
+  var date = new Date(this.valueOf())
+  var res = time.trim().match(/(\d+)/g)
+  hours = parseInt(res[0])
+  minutes = parseInt(res[1])
+  seconds = parseInt(res[2])
+  date.setHours(date.getHours()+hours)
+  date.setMinutes(date.getMinutes()+minutes)
+  date.setSeconds(date.getSeconds()+seconds)
+  return date
+}
 
 function GM_addStyle(cssStr) {
     var D = document;
@@ -29,13 +39,23 @@ GM_addStyle(`
 `)
 
 const table_rows = $("#runTable tbody tr")
-const data_atual = new Date()
+const current_date = new Date()
 
 for(i=0;i<table_rows.length;i++){
   if(table_rows[0].className == ""){
-    if(new Date($(table_rows[i]).children('td.start-time').html()) > data_atual){
+    var run_time = new Date($(table_rows[i]).children('td.start-time').html())
+    if(run_time > current_date){
       $(table_rows[i-2]).addClass("current").attr('id', 'current').next().addClass("current")
       break
+    }
+  }
+  
+  if(i==table_rows.length-1){
+    console.log("ultima linha")
+    run_time = new Date($(table_rows[i-1]).children('td.start-time').html())
+    run_time = run_time.sum_str($(table_rows[i]).children("td.text-right").text())    
+    if(current_date < run_time){
+      $(table_rows[i-1]).addClass("current").attr('id','current').next().addClass("current")
     }
   }
 }
